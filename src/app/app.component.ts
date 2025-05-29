8f1b2a816b0cbb32ba5255cec3654589ebc2c3d1
shivamjain0320 <shivamjain0320@gmail.com>
2025-05-30 00:58:16 +0530

    feat: home page initilization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +7,11 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Assignment';
+
+  // Sample movie data
+  ngOnInit(): void {
+      console.log("");
+  }
 }
