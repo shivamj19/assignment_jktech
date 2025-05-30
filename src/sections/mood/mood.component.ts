87d9d64632b16864d9369fea74bdb6bdbb87be6e
shivamjain0320 <shivamjain0320@gmail.com>
2025-05-31 00:18:30 +0530

    feat: component and section division added

---
+++ src/sections/mood/mood.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mood',
+  imports: [],
+  templateUrl: './mood.component.html',
+  styleUrl: './mood.component.css'
+})
+export class MoodComponent {
+
+}
---
+++ src/sections/popular-movies/popular-movies.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-popular-movies',
+  imports: [],
+  templateUrl: './popular-movies.component.html',
+  styleUrl: './popular-movies.component.css'
+})
+export class PopularMoviesComponent {
+
+}
