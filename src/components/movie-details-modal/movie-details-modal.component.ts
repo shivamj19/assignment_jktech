973c5f71b4e5f52f1aea531b7c7c1d9b1d871ef9
Shivam Jain <65396029+shivamj19@users.noreply.github.com>
2025-05-30 18:00:45 +0530

    components added for movie card and modal

---
+++ src/components/movie-details-modal/movie-details-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-details-modal',
+  imports: [],
+  templateUrl: './movie-details-modal.component.html',
+  styleUrl: './movie-details-modal.component.css'
+})
+export class MovieDetailsModalComponent {
+
+}
